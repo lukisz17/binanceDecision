7b9d975e2d19e04e193868f6d389d16fbd367586
lukisz17 <lukasz.szamalek@gmail.com>
2021-12-25 16:58:14 +0100

    init

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pandas_ta as ta
 from flask import Flask, request
 from flask_restful import Api, Resource
 from unicorn_binance_rest_api.unicorn_binance_rest_api_manager import BinanceRestApiManager
@@ -13,8 +14,13 @@ class DecisionRsponse(Resource):
         return {"status": "Please use POST instead"}
 
     def post(self):
-        klines = request.json['klines']
-        print(klines[49]['closeTimeString'])
+        klines = np.array(request.json['klines'])
+        df = createDataFrame(klines, False)
+        adx = ta.adx(close=df['close'], high=df['high'], low=df['low'])
+        df['DMN_14_24'] = ta.ema(adx['DMN_14'], length=24)  # to 14 i  24 musi byc parametrem
+        df2 = ta.psar(df['DMN_14_24'], df['DMN_14_24'])  # tutaj to tez musza byc parametry
+        df['PSARl_0.02_0.2'] = df2['PSARl_0.02_0.2']
+        print(df)
         return {"status": "OK"}
 
 api.add_resource(DecisionRsponse, "/decision")
@@ -29,65 +35,21 @@ def getKLines(apiManager, symbol, interval):
     return data
 
 
-def createDataFrame(data):
-    minPrice = 1000000
-    maxPrice = 0
-
-    OPEN_PRICE_INDEX = 1
-    HIGH_PRICE_INDEX = 2
-    LOW_PRICE_INDEX = 3
-    CLOSE_PRICE_INDEX = 4
-    VOLUME_INDEX = 5
-    QUOTE_VOLUME_INDEX = 7
-    TAKER_BUY_VOLUME_INDEX = 9
-    TAKER_BUY_QUOTE_VOLUME_INDEX = 10
+def createDataFrame(data, isColab):
 
     dict = []
 
-    MFI_LEN = 10  # 14
-
-    for i in range(MFI_LEN, len(data)):
+    for i in range(len(data)):
 
         element = data[i]
-        closeTime = int(element[0]) / 1000
-        volume = float(element[VOLUME_INDEX])
-        takerBuyVolume = float(element[TAKER_BUY_VOLUME_INDEX])
-        close = float(element[CLOSE_PRICE_INDEX])
-        open = float(element[OPEN_PRICE_INDEX])
-        low = float(element[LOW_PRICE_INDEX])
-        high = float(element[HIGH_PRICE_INDEX])
-
-        demandMoneyFlow = 0
-        supplyMoneyFlow = 0
-
-        for j in range(i - MFI_LEN + 1, i + 1):
-            elem = data[j]
-            elemPrev = data[j - 1]
-
-            typicalPrice = (float(elem[CLOSE_PRICE_INDEX]) + float(elem[LOW_PRICE_INDEX]) + float(
-                elem[HIGH_PRICE_INDEX])) / 3
-
-            if typicalPrice < minPrice:
-                minPrice = typicalPrice
-            if typicalPrice > maxPrice:
-                maxPrice = typicalPrice
-
-            # ---
-            vol = float(elem[VOLUME_INDEX])
-            demandVol = float(elem[TAKER_BUY_VOLUME_INDEX])
-            supplyVol = vol - demandVol
-
-            demandMoneyFlow += demandVol * typicalPrice
-            supplyMoneyFlow += supplyVol * typicalPrice
-
-        demandSupplyMoneyRatio = 0
-
-        if supplyMoneyFlow > 0:
-            demandSupplyMoneyRatio = demandMoneyFlow / supplyMoneyFlow
-        else:
-            demandSupplyMoneyRatio = 0
-
-        MFI_SD = (100 - (100 / (1 + demandSupplyMoneyRatio)))
+        closeTime = int(element['closeTime']) / 1000
+        volume = float(element['volume'])
+        takerBuyVolume = float(element['takerBuyBaseAssetVolume'])
+        close = float(element['close'])
+        open = float(element['open'])
+        low = float(element['low'])
+        high = float(element['high'])
+        hlc3 = float((high + low + close)/3.0)
 
         dict.append({
             'closeTime': datetime.fromtimestamp(closeTime).strftime('%Y-%m-%d %H:%M:%S'),
@@ -95,13 +57,12 @@ def createDataFrame(data):
             'high': high,
             'low': low,
             'close': close,
-            'MFI_SD': MFI_SD,
-            'typicalPrice': typicalPrice
+            'hlc3': hlc3
         })
 
     dataFrame = pd.DataFrame(dict)
     dataFrame['closeTime'] = pd.to_datetime(dataFrame['closeTime'])
-    return dataFrame, minPrice, maxPrice
+    return dataFrame
 
 @app.route('/')
 def hello_world():
@@ -111,7 +72,8 @@ def hello_world():
 def calculate():
     apiManager = BinanceRestApiManager('api_key', 'api_secret', exchange="binance.com")
     data = np.array(getKLines(apiManager, "ETHUSDT", '1m'))
-    df, minPrice, maxPrice = createDataFrame(data)
+    df = createDataFrame(data, False)
+    print(df)
     return str(df.size)
 
 if __name__ == '__main__':
