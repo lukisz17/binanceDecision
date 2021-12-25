54bc4490f1c969e2d0b35141efd95f7071e9b60f
lukisz17 <lukasz.szamalek@gmail.com>
2021-12-25 14:02:30 +0100

    init

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask
+from unicorn_binance_rest_api.unicorn_binance_rest_api_manager import BinanceRestApiManager
+import numpy as np
 
 app = Flask(__name__)
 
 
+def getKLines(apiManager, symbol, interval):
+    data = apiManager.get_historical_klines(
+        symbol=symbol,
+        interval=interval,
+        limit=200
+    )
+
+    return data
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    apiManager = BinanceRestApiManager('api_key', 'api_secret', exchange="binance.com")
+    data = np.array(getKLines(apiManager, "ETHUSDT", '1m'))
+    return 'Hello World!' + len(data)
 
 
 if __name__ == '__main__':
